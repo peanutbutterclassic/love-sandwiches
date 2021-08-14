4ba12b0693f6628169eab6d125d855696f4e2fdc
peanutbutterclassic <rano.r.s.fisher@gmail.com>
2021-08-14 10:42:20 +0000

    Create credentials

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ SCOPE = [
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPEAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPEAD_CLIENT.open('love_sandwiches')
+SHEET = GSPEAD_CLIENT.open('love_sandwiches') 
 
 def get_sales_data():
     """
@@ -31,6 +31,7 @@ def validate_data(values):
     or if there aren't exactly six values.
     """
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}")
